8af6d417fbb376f6fc4f2dd3e0878c62374bc1b4
Valtteri Nummi <valtteri.nummi@gmail.com>
2024-12-10 21:46:23 +0200

    feat: added load json method

--- utils/support.py
+++ utils/support.py
@@ -2,8 +2,8 @@ from inspect import stack
 from os import walk
 from pygame import Surface
 from pygame.image import load as load_image
-from typing import Optional
-
+from typing import Optional, Union
+from json import load as _load
 
 def get_graphics_images_from_folder(folder_path: str) -> Optional[list[Surface]]:
     try:
@@ -19,6 +19,23 @@ def get_graphics_images_from_folder(folder_path: str) -> Optional[list[Surface]]
     except Exception as error:
         error_name: str = type(error).__name__
         print(f'[Exception]::[{error_name} occured in import_graphics_images_from_folder: {error}]')
+        for item in stack():
+            print(item.function)
+        return None
+    
+
+def read_item_data_from_json(data_type: str) -> Optional[list[dict[str, Union[str, int]]]]:
+    try:
+        file_path: str = f'./data/item_data/{data_type}.json'
+
+        with open(file=file_path, mode='r') as file:
+            data = _load(file)
+
+            return data
+
+    except Exception as error:
+        error_name: str = type(error).__name__
+        print(f'[Exception]::[{error_name} occured in read_item_data_from_json: {error}]')
         for item in stack():
             print(item.function)
         return None
